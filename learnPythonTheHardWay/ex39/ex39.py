3f408bbe09bdf98c2d1354d5ac7c43a731159815
bllanos <llanbe_t3mp@yahoo.ca>
2015-04-06 21:55:19 -0400

    LPTHW Exercise 39 in progress

---
+++ learnPythonTheHardWay/ex39/ex39.py
@@ -0,0 +1,61 @@
+# Create a mapping of state to abbreviation
+states = {
+    'Oregon': 'OR',
+    'Florida': 'FL',
+    'California': 'CA',
+    'New York': 'NY',
+    'Michigan': 'MI'
+}
+    
+# Create a basic set of states and some cities in them
+cities = {
+    'CA': 'San Francisco',
+    'MI': 'Detroit',
+    'FL': 'Jacksonville'
+}
+
+# Add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+# Print out some cities
+print '-' * 10
+print "NY State has: ", cities['NY']
+print "OR State has: ", cities['OR']
+
+# Print some states
+print '-' * 10
+print "Michigan's abbreviation is: ", states['Michigan']
+print "Florida's abbreviation is: ", states['Florida']
+
+# Do it by using the state then cities dicts
+print '-' * 10
+print "Michigan has: ", cities[states['Michigan']]
+print "Florida has: ", cities[states['Florida']]
+
+# Print every state abbreviation
+print '-' * 10
+for state, abbrev in states.items(): # Returns a copy of the list of pairs in the dict
+    print "%s is abbreviated %s" % (state, abbrev)
+    
+# Print every city in state
+print '-' * 10
+for abbrev, city in cities.items():
+    print "%s has the city %s" % (abbrev, city)
+    
+# Both at the same time
+print '-' * 10
+for state, abbrev in states.items():
+    print "%s state is abbreviated %s and has city %s" % (
+        state, abbrev, cities[abbrev])
+        
+print '-' * 10
+# Will not throw a KeyError if the mapping does not exist
+state = states.get("Texas")
+
+if not state:
+    print "Texas is not mapped."
+    
+# Get a city with a default value
+city = cities.get('TX', "Does not Exist")
+print "The city for the state 'TX' is: %s" % city
