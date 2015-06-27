0302244ed2e42881ceec4d2c919dc224b1dea18a
bllanos <llanbe_t3mp@yahoo.ca>
2015-06-27 10:54:53 -0400

    Verbs test completed.

--- learnPythonTheHardWay/ex48/ex48/lexicon.py
+++ learnPythonTheHardWay/ex48/ex48/lexicon.py
@@ -7,25 +7,44 @@ def scan(sentence):
     # Initialization
     pairs = ((None, word, i) for i, word in enumerate(sentence.split()))
     output_pairs = []
-    directions, pairs = test_directions(pairs)
-    output_pairs.extend(directions) # Concatenation
 
+    # Check for each type of token
+    for test in LEXICON_TESTS:
+        identified, pairs = test(pairs)
+        output_pairs.extend(identified) # Concatenation
+
+    # Ideally, I would have concatenated the lists into a sorted set,
+    # such that sorting here would not be necessary.
+    # Of course, for small input sizes, it doesn't matter.
+    output_pairs.extend(pairs)
     output_pairs = sorted(output_pairs, key=itemgetter(2))
     output_pairs = [(token, word) for token, word, i in output_pairs]
     return output_pairs
 
 DIRECTIONS = frozenset(['north', 'south', 'east', 'west'])
-direction_token = 'direction'
+VERBS = frozenset(['go', 'stop', 'kill', 'eat'])
+DIRECTION_TOKEN = 'direction'
+VERB_TOKEN = 'verb'
 
-def test_directions(pairs):
-    directions = []
-    output_pairs = []
-    for pair in pairs:
-        if pair[1] in DIRECTIONS:
-            directions.append((direction_token, pair[1], pair[2]))
-        else:
-            output_pairs.append(pair)
+LEXICON = {
+    DIRECTION_TOKEN: DIRECTIONS,
+    VERB_TOKEN: VERBS
+}
 
-    return directions, output_pairs
+def make_test(token_name, whiteset):
 
-__all__ = [scan]
+    def test_pairs(pairs):
+        passed = []
+        failed = []
+        for pair in pairs:
+            if pair[1] in whiteset:
+                passed.append((token_name, pair[1], pair[2]))
+            else:
+                failed.append(pair)
+        return passed, failed
+
+    return test_pairs
+
+LEXICON_TESTS = [make_test(*item) for item in LEXICON.iteritems()]
+
+__all__ = ['scan']
