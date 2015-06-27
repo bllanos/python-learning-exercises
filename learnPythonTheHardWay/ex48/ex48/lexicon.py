6265549ba005231f72c5b17d315205209dc9a7e3
bllanos <llanbe_t3mp@yahoo.ca>
2015-06-27 11:33:49 -0400

    LPTHW Exercise 48 completed.

--- learnPythonTheHardWay/ex48/ex48/lexicon.py
+++ learnPythonTheHardWay/ex48/ex48/lexicon.py
@@ -5,7 +5,7 @@ def scan(sentence):
     tuples
     """
     # Initialization
-    pairs = ((None, word, i) for i, word in enumerate(sentence.split()))
+    pairs = ((ERROR_TOKEN, word, i) for i, word in enumerate(sentence.split()))
     output_pairs = []
 
     # Check for each type of token
@@ -23,12 +23,20 @@ def scan(sentence):
 
 DIRECTIONS = frozenset(['north', 'south', 'east', 'west'])
 VERBS = frozenset(['go', 'stop', 'kill', 'eat'])
+STOPS = frozenset(['the', 'in', 'of', 'from', 'at', 'it'])
+NOUNS = frozenset(['door', 'bear', 'princess', 'cabinet'])
 DIRECTION_TOKEN = 'direction'
 VERB_TOKEN = 'verb'
+STOP_TOKEN = 'stop'
+NOUN_TOKEN = 'noun'
+NUMBER_TOKEN = 'number'
+ERROR_TOKEN = 'error'
 
 LEXICON = {
     DIRECTION_TOKEN: DIRECTIONS,
-    VERB_TOKEN: VERBS
+    VERB_TOKEN: VERBS,
+    STOP_TOKEN: STOPS,
+    NOUN_TOKEN: NOUNS
 }
 
 def make_test(token_name, whiteset):
@@ -46,5 +54,18 @@ def make_test(token_name, whiteset):
     return test_pairs
 
 LEXICON_TESTS = [make_test(*item) for item in LEXICON.iteritems()]
+
+def test_numbers(pairs):
+    passed = []
+    failed = []
+    for pair in pairs:
+        try:
+            n = int(pair[1])
+            passed.append((NUMBER_TOKEN, n, pair[2]))
+        except ValueError:
+            failed.append(pair)
+    return passed, failed
+
+LEXICON_TESTS.append(test_numbers)
 
 __all__ = ['scan']
--- learnPythonTheHardWay/ex48/tests/ex48_tests.py
+++ learnPythonTheHardWay/ex48/tests/ex48_tests.py
@@ -14,3 +14,33 @@ def test_verbs():
     assert_equal(result, [('verb', 'go'),
                           ('verb', 'kill'),
                           ('verb', 'eat')])
+
+def test_stops():
+    assert_equal(lexicon.scan("the"), [('stop', 'the')])
+    result = lexicon.scan("the in of")
+    assert_equal(result, [('stop', 'the'),
+                          ('stop', 'in'),
+                          ('stop', 'of')])
+
+def test_nouns():
+    assert_equal(lexicon.scan("bear"), [('noun', 'bear')])
+    result = lexicon.scan('bear princess')
+    assert_equal(result, [('noun', 'bear'),
+                          ('noun', 'princess')])
+
+def test_numbers():
+    assert_equal(lexicon.scan("1234"), [('number', 1234)])
+    result = lexicon.scan("3 91234")
+    assert_equal(result, [('number', 3),
+                          ('number', 91234)])
+
+def test_errors():
+    assert_equal(lexicon.scan("ASDFANNN"), [('error', 'ASDFANNN')])
+    result = lexicon.scan("bear IAS princess go in 14 east")
+    assert_equal(result, [('noun', 'bear'),
+                          ('error', 'IAS'),
+                          ('noun', 'princess'),
+                          ('verb', 'go'),
+                          ('stop', 'in'),
+                          ('number', 14),
+                          ('direction', 'east')])
