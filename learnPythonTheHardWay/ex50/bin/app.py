bd736119fcbe228c1d4f24c5d009e9b65ffaae25
bllanos <llanbe_t3mp@yahoo.ca>
2015-06-28 11:52:48 -0400

    LPTHW Exercise 50 completed.

--- learnPythonTheHardWay/ex50/bin/app.py
+++ learnPythonTheHardWay/ex50/bin/app.py
@@ -6,10 +6,13 @@ urls = (
 
 app = web.application(urls, globals())
 
+render = web.template.render('templates/')
+
 class Index(object):
     def GET(self):
         greeting = "Hello World"
-        return greeting
+        # Using keyword arguments
+        return render.index(greeting=greeting)
 
 if __name__ == "__main__":
     app.run()
