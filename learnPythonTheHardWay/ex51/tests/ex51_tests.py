8e6701735d257cd726f1d55ba79e99a03ea1d314
bllanos <llanbe_t3mp@yahoo.ca>
2015-07-09 20:31:32 -0400

    LPTHW Exercise 51 complete

--- learnPythonTheHardWay/ex51/tests/ex51_tests.py
+++ learnPythonTheHardWay/ex51/tests/ex51_tests.py
@@ -1,4 +1,22 @@
 from nose.tools import *
+from bin.app import app
+from tests.tools import assert_response # Using an absolute import here
 
-def test_blank():
-    pass
+def test_index():
+    # check that we get a 404 on the / URL
+    resp = app.request("/")
+    assert_response(resp, status="404")
+    
+    # test initial GET request to /hello
+    resp = app.request("/hello")
+    assert_response(resp)
+    
+    # make sure default values work for the form
+    resp = app.request("/hello", method="POST")
+    assert_response(resp, contains="Nobody")
+    
+    # test that parameters can be passed
+    data = {'name': 'self', 'msg': 'Hola'}
+    resp = app.request("/hello", method="POST", data=data)
+    assert_response(resp, contains="self")
+    assert_response(resp, contains="Hola")
